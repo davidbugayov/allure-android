f9bc0700874886b24ae2938da2d0a50d8a08da7d
BugaevDavidS <DSBugaev.SBT@sberbank.ru>
2018-06-29 15:56:06 +0300

    fix step for test

--- allure-android/src/main/kotlin/ru/tinkoff/allure/android/Step.kt
+++ allure-android/src/main/kotlin/ru/tinkoff/allure/android/Step.kt
@@ -8,24 +8,6 @@ import ru.tinkoff.allure.model.*
 /**
  * @author b.mukvich on 31.05.2017.
  */
-
-inline fun <T : Any?> step(description: String, vararg params: Parameter, block: () -> T): T {
-    with(Step()) {
-        val result: T
-        stepStart(description, *params)
-        try {
-            result = block()
-            stepCompleted()
-        } catch (t: Throwable) {
-            stepThrown(t)
-            throw t
-        } finally {
-            stepStop()
-        }
-        return result
-    }
-}
-
 class Step {
     // todo: commonLifecycle works on Android, only 'cause steps don't call writer
     private val lifecycle: AllureLifecycle = AllureCommonLifecycle
@@ -73,11 +55,11 @@ class Step {
 /**
  * Метод для вызова шага с скриншотом
  */
-fun stepWithScreen(description: String, parameters: Parameters.Builder? = null, block: Runnable){
-    return stepWithScreen(description,parameters) { block.run() }
+fun stepWithScreenShot(description: String, parameters: Parameters.Builder? = null, block: Runnable){
+    return stepWithScreenShot(description,parameters) { block.run() }
 }
 
-fun stepWithScreen(description: String, parameters: Parameters.Builder? = null, block: () -> Unit) {
+private fun stepWithScreenShot(description: String, parameters: Parameters.Builder? = null, block: () -> Unit) {
     with(Step()) {
         if (parameters != null) {
             stepStart(description, *parameters.build().parameters)
@@ -100,11 +82,11 @@ fun stepWithScreen(description: String, parameters: Parameters.Builder? = null, 
 /**
  * Метод для вызова шага без скриншота
  */
-fun  stepWithoutScreen(description: String, parameters: Parameters.Builder? = null, block: Runnable) {
-    return stepWithoutScreen(description,parameters) { block.run() }
+fun  step(description: String, parameters: Parameters.Builder? = null, block: Runnable) {
+    return step(description,parameters) { block.run() }
 }
 
-fun stepWithoutScreen(description: String, parameters: Parameters.Builder? = null, block: () -> Unit) {
+private fun step(description: String, parameters: Parameters.Builder? = null, block: () -> Unit) {
     with(Step()) {
         if (parameters != null) {
             stepStart(description, *parameters.build().parameters)
